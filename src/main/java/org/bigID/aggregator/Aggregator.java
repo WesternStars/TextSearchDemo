897342cb78651aaa64b167980a7d0230534ee80d
Artem Puhachov <puhachov.artem@gmail.com>
2024-10-17 00:29:27 +0300

    Update Aggregator

--- src/main/java/org/bigID/aggregator/Aggregator.java
+++ src/main/java/org/bigID/aggregator/Aggregator.java
@@ -8,41 +8,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.flatMapping;
 import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
 
+// todo Interface and Impl for provider
 public class Aggregator {
 
-    public Map<String, List<Position>> aggregateByName(List<Map<String, List<Position>>> futures) {
-        return sortPositions(groupingByName(futures));
-    }
-
-    private static Map<String, List<Position>> sortPositions(Map<String, List<Stream<Position>>> streamsGroupedByName) {
-        return streamsGroupedByName.entrySet().stream()
-                .map(entry -> Map.entry(entry.getKey(), sortPositions(entry.getValue())))
-                .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
-    }
-
-    private static Map<String, List<Stream<Position>>> groupingByName(List<Map<String, List<Position>>> futures) {
-        return futures.stream()
+    public Map<String, List<Position>> aggregateByName(List<Map<String, List<Position>>> results) {
+        return results.stream()
                 .map(Map::entrySet)
                 .flatMap(Collection::stream)
-                .collect(groupingBy(Entry::getKey, mapping(getMapper(), toList())));
-    }
-
-    private static List<Position> sortPositions(List<Stream<Position>> entry) {
-        return entry.stream()
-                .flatMap(identity())
-                .sorted(Comparator.comparingInt(Position::lineOffset))
-                .toList();
+                .collect(groupingBy(Entry::getKey, flatMapping(getMapper(), toList())));
     }
 
     private static Function<Entry<String, List<Position>>, Stream<Position>> getMapper() {
-        return e -> e.getValue().stream();
+        return e -> e.getValue().stream()
+                .sorted(Comparator.comparingInt(Position::lineOffset));
     }
 }
